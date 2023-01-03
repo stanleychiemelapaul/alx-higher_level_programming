18d389ba7b54825fbe3ea738eca82ca21c5ef065
Stanley Chiemela Paul <stanleypaul313@gmail.com>
2023-01-03 02:27:50 -0800

    Adding more files

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print("".join(chr(i) for i in range(97, 123) if chr(i) not in ['q', 'e']), end="")
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print("\n".join("{:2d} = 0x{:2x}".format(i, i) for i in range(99)))
