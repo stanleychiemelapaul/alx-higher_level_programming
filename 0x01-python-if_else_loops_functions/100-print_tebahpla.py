b105772001c0da6d6575b0d1a49dab5b6ce7a498
Stanley Chiemela Paul <42416676+stanleychiemelapaul@users.noreply.github.com>
2023-01-03 12:53:50 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(26):
-    if i % 2 == 0:
-        print(chr(ord("z") - i), end="")
-    else:
-        print(chr(ord("Z") - i), end="")
+for ch in reversed(range(97, 123)):
+    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
