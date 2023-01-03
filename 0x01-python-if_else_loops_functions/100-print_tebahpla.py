75ba7659c568a05ecc0ba7a549dd22b13265d2e1
Stanley Chiemela Paul <42416676+stanleychiemelapaul@users.noreply.github.com>
2023-01-03 12:52:36 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-print("".join(chr(ord("z") - i) if i % 2 == 0 else chr(ord("Z") - i) for i in range(26)), end="")
+for i in range(26):
+    if i % 2 == 0:
+        print(chr(ord("z") - i), end="")
+    else:
+        print(chr(ord("Z") - i), end="")
