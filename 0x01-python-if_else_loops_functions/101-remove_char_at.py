9b1f3fb2cad0490a9f4b487f97e58ecc1278d004
Stanley Chiemela Paul <42416676+stanleychiemelapaul@users.noreply.github.com>
2023-01-03 12:42:17 +0100

    Update 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,10 @@
+# #!/usr/bin/python3
+# def remove_char_at(str, n):
+#     return str[:n] + str[n+1:]
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    return str[:n] + str[n+1:]
+    s = ""
+    for i in range(len(str)):
+        if i != n:
+            s = s + str[i]
+    return (s)
